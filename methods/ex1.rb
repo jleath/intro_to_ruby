2655529dfd1ee18401728dc4ce0214abc59408ba
Josh Leath <joshua.leath@icloud.com>
2020-08-19 17:51:55 -0400

    completed methods chapter

---
+++ methods/ex1.rb
@@ -0,0 +1,10 @@
+# Write a program that prints a greeting message. This program should
+# contain a method called greeting that takes a name as its  parameter
+# and returns a string
+
+def greeting(name)
+  return "Hello, #{name}"
+end
+
+puts greeting("Josh")
+
---
+++ methods/ex4.rb
@@ -0,0 +1,7 @@
+# What will the following code print to the screen?
+
+def scream(words)
+  puts words = words + "!!!!"
+end
+
+scream("Yippeee")
