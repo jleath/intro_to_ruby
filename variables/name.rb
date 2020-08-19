6e611709fda9333d1f1bde012acba3d3dc2cb835
Josh Leath <joshua.leath@icloud.com>
2020-08-19 14:57:20 -0400

    completed variables chapter

---
+++ variables/name.rb
@@ -0,0 +1,10 @@
+# Write a program called name.rb that asks the user to type in their name
+# and then prints out a greeting message with their name included.
+
+puts "What is your first name?"
+first_name = gets.chomp
+puts "What is your last name?"
+last_name = gets.chomp
+puts "Hello, #{first_name} #{last_name}!"
+
+10.times { puts "#{first_name} #{last_name}" }
